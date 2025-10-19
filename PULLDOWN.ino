c412bd2a2f2f19732f7852bef09d0dd2c7a93d52
ZackkkkEET <2371658@edu.vaniercollege.qc.ca>
2025-10-18 21:50:30 -0400

    Add files via upload

---
+++ PULLDOWN.ino
@@ -0,0 +1,21 @@
+//THEME: Pulldown 
+
+//The goal is when PB is not pressed, value is 0, when pressed = 1
+
+int pushButton = 2;
+void setup() {  
+// the setup routine runs once when you press reset:  
+
+Serial.Begin(9600);  // initialize serial communication at 9600 bits per second:   
+pinMode(2, INPUT_PULLDOWN);  //sets pin 2 as input pull down
+}
+
+void loop() {     
+// the loop routine runs over and over again forev 
+int buttonState = digitalRead(pushbutton); //reads val of PB pin 2
+Serial.print("buttonState = "); //prints buttonState = 
+Serial.println(buttonState); //prints the val of PB
+delay (1000); //waits 1 second
+} 
+
+ 
